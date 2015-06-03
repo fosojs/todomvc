7f86b53bd41039452f25086bba59496883b28541
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-06-04 01:21:01 +0300

    fosify used for bundling less files.

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,10 @@
 var gulp       = require('gulp');
 var util       = require('gulp-util');
 //var imagemin   = require('gulp-imagemin');
-var less       = require('gulp-less');
 var jshint     = require('gulp-jshint');
 var dotify     = require('gulp-dotify');
 var header     = require('gulp-header');
 var footer     = require('gulp-footer');
-var minifyCSS  = require('gulp-minify-css');
 var minifyHTML = require('gulp-minify-html');
 var concat     = require('gulp-concat');
 var fosify = require('fosify');
@@ -24,15 +22,6 @@ gulp.task('lint', function() {
     gulp.src(dir.src + 'js/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
-});
-
-gulp.task('css', function() {
-    var destination = (util.env.production ? dir.prod : dir.dev) + 'css/';
-
-    gulp.src(dir.src + 'css/base.less')
-        .pipe(less())
-        .pipe(util.env.production ? minifyCSS() : util.noop())
-        .pipe(gulp.dest(destination));
 });
 
 gulp.task('images', function() {
@@ -61,10 +50,10 @@ gulp.task('scripts', function() {
     var destination = (util.env.production ? dir.prod : dir.dev) + 'js/';
 
     fosify({
-      source: dir.src + 'js',
+      src: dir.src,
       dest: destination,
       minify: util.env.production,
-      watch: false
+      watch: !util.env.production
     });
 });
 
@@ -86,12 +75,10 @@ gulp.task('resources', function() {
 gulp.task('watch', function() {
     if (util.env.production) { return; }
 
-    gulp.watch(dir.src + 'css/**/*.less', ['css']);
     gulp.watch(dir.src + 'img/**/*', ['images']);
     gulp.watch(dir.src + 'js/templates/**/*.html', ['templates']);
-    gulp.watch(dir.src + 'js/**/*.js', ['scripts']);
     gulp.watch(dir.src + 'index.html', ['resources']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['css', 'images', 'templates', 'scripts', 'resources', 'watch']);
+gulp.task('default', ['images', 'templates', 'scripts', 'resources', 'watch']);
