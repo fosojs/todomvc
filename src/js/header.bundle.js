5f1c2f81eef1a91870234c3d416de98b9df2400d
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-08-30 19:52:48 +0300

    kibe updated to v0.2.0

--- src/js/header.bundle.js
+++ src/js/header.bundle.js
@@ -2,15 +2,10 @@
 
 var kibe = require('kibe');
 kibe({
-  abo: function(mode) {
-    if (mode === 'abodev') {
-      return location.protocol === 'http:' ?
-        'http://localhost:1769/index.js' : 'https://localhost:1770/index.js';
-    }
-    if (mode === 'aboprod') {
-      return 'http://localhost:9595/dev/todomvc-ab/index.js';
-    }
-  }
+  abodev: kibe.js('abo', location.protocol === 'http:' ?
+    'http://localhost:1769/index.js' :
+    'https://localhost:1770/index.js'),
+  aboprod: kibe.js('abo', 'http://localhost:9595/dev/todomvc-ab/index.js')
 });
 
 window.kibe = kibe;
