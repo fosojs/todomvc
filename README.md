f2f49dc2aa888bc66b07fc81cee8360f7569c3c5
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-12-19 19:58:57 +0200

    ejs used for the TodoMVC template.

--- README.md
+++ README.md
@@ -13,10 +13,24 @@ This does require that nodeJS and gulp be installed.
 $ git clone git://github.com/fosojs/todomvc.git
 # install dependent plugins
 $ cd todomvc
-$ npm install     # Install gulp/foso dev dependencies into /node_modules
-$ bower install   # Install front-end resources from bower into /bower_components
+$ npm install # Install gulp dev dependencies into /node_modules
+$ cd assets # Going to the assets project
+$ npm install
 ```
+
+
+## Usage
+
+In order to start developing the website, run `gulp develop` in the root directory
+of the project. This command will render the page and bundle the assets.
+
+In order to generate a production ready page, run `gulp publish`. The rendered page
+will reference resources hosted on the [foso-cdn][].
+
+To publish a new version of the assets, just publish a new version of the assets' npm module.
+
 
 [todoMVC]: http://addyosmani.github.com/todomvc
 [gulp]: https://github.com/gulpjs/gulp
 [foso]: https://github.com/fosojs/foso
+[foso-cdn]: https://github.com/fosojs/cdn
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,35 @@
 'use strict';
 
 const gulp = require('gulp');
+const ejs = require('gulp-ejs');
 const hub = require('gulp-hub');
-const Foso = require('foso');
-const html = require('fosify-html');
+const gutil = require('gulp-util');
+const webserver = require('gulp-webserver');
 
-gulp.task('develop', function(cb) {
-  let foso = new Foso();
-  foso
-    .register([html], {
-      basePath: __dirname,
-      serve: {
-        port: 5000,
-      },
-      watch: true,
-      livereload: {
-        port: 50123,
-      },
-    })
-    .then(() => foso.bundle())
-    .then(cb)
-    .catch(cb);
+const destination = './dist';
+
+function render(model) {
+  gulp.src('./views/*.ejs')
+    .pipe(ejs(model).on('error', gutil.log))
+    .pipe(gulp.dest(destination));
+}
+
+gulp.task('publish', function() {
+  render({
+    cdnOrigin: 'http://cdn.foso.me',
+  });
+});
+
+gulp.task('develop', function() {
+  render({
+    cdnOrigin: 'http://localhost:9595',
+  });
+
+  gulp.src(destination)
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+    }));
 });
 
 gulp.task('default', ['develop']);
